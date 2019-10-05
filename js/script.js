6c8122b11c500bcae49841c3483d7326e09c481e
Slava Kolytchev <megalo@mail.ru>
2019-10-05 18:14:25 +0300

    Новая задача

--- js/script.js
+++ js/script.js
@@ -5,8 +5,20 @@ let mainList = {
     employers: {},
     open: false
 };
+let i = 0;
+do {
+    let a = prompt("Что будем продавать?");
+    console.log(typeof a);
+    if (isNaN(+a) && (a != null && a != "" && a.length < 50)) {
+        mainList.shopGoods[i] = a;
+        console.log("Все ОК");
+    } else {
+        console.log("Not OK");
+        i--;
+    }
+    i++;
+} while (i < 5);
 
-mainList.shopGoods[0] = prompt("Что будем продавать?");
 mainList.shopGoods[1] = prompt("Что будем продавать?");
 mainList.shopGoods[2] = prompt("Что будем продавать?");
-document.write("Ваш бюджет на 1 день - " + mainList.m / 30);
+document.write("Ваш бюджет на 1 день - " + mainList.money / 30);
